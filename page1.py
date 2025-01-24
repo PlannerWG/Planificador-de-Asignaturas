0dbd1e89b23e77c1f91d13fa7a28e090fa40fec4
Alonso <planner.wg@gmail.com>
2025-01-24 10:50:18 -0300

    $@

--- page1.py
+++ page1.py
@@ -3,41 +3,20 @@ import streamlit as st
 st.title("Selecciona tu Escuela y Carrera")
 
 # Lista de escuelas y carreras por escuela
+# En la lista que son los valores del dict se pueden agregar las carreras a medida que las tengamos.
 escuelas = {
     "Escuela de Ingeniería": ["Ingeniería Civil en Computación"],
 }
+escuela_seleccionada = st.selectbox("Selecciona una escuela", ["Selecciona una opción"] + list(escuelas.keys()))
 
-# Inicializar estado para la selección
-if "escuela_seleccionada" not in st.session_state:
-    st.session_state.escuela_seleccionada = "Selecciona una opción"
-
-# Menú desplegable de escuelas
-escuela_seleccionada = st.selectbox(
-    "Selecciona una escuela",
-    [st.session_state.escuela_seleccionada] + list(escuelas.keys()),
-)
-
-# Actualizar la selección de la escuela en el estado
-if escuela_seleccionada != "Selecciona una opción":
-    st.session_state.escuela_seleccionada = escuela_seleccionada
+carreras = []
 
 # Mostrar el segundo menú si se selecciona una escuela
 if escuela_seleccionada != "Selecciona una opción":
     carreras = escuelas[escuela_seleccionada]
+    carrera_seleccionada = st.selectbox("Selecciona una carrera", ["Selecciona una opción"] + carreras)
 
-    # Inicializar estado para la selección de carrera
-    if "carrera_seleccionada" not in st.session_state:
-        st.session_state.carrera_seleccionada = "Selecciona una opción"
-
-    carrera_seleccionada = st.selectbox(
-        "Selecciona una carrera",
-        [st.session_state.carrera_seleccionada] + carreras,
-    )
-
-    # Actualizar la selección de la carrera en el estado
     if carrera_seleccionada != "Selecciona una opción":
-        st.session_state.carrera_seleccionada = carrera_seleccionada
         st.success(f"Has seleccionado: {escuela_seleccionada} - {carrera_seleccionada}")
-
 else:
     st.warning("Por favor, selecciona una escuela para continuar.")
