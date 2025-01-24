5453f887829d23144be767416c75d93de2be545f
Alonso <planner.wg@gmail.com>
2025-01-24 11:36:10 -0300

    $@

--- page1.py
+++ page1.py
@@ -15,14 +15,14 @@ st.title("Selecciona tu Escuela y Carrera")
 escuelas = {
     "Escuela de Ingeniería": ["Ingeniería Civil en Computación"],
 }
-escuela_seleccionada = st.selectbox("Selecciona una escuela", [""] + list(escuelas.keys()))
+escuela_seleccionada = st.selectbox("Selecciona una escuela", ["Selecciona una opción"] + list(escuelas.keys()))
 
 carreras = []
 
 # Mostrar el segundo menú si se selecciona una escuela
 if escuela_seleccionada != "Selecciona una opción":
     carreras = escuelas[escuela_seleccionada]
-    carrera_seleccionada = st.selectbox("Selecciona una carrera", [""] + carreras)
+    carrera_seleccionada = st.selectbox("Selecciona una carrera", ["Selecciona una opción"] + carreras)
 
     if carrera_seleccionada != "Selecciona una opción":
         st.success(f"Has seleccionado: {escuela_seleccionada} - {carrera_seleccionada}")
