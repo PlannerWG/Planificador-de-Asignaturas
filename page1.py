57b3e2445ea8fbddf2e821c2e0ff38281b50903f
Alonso <planner.wg@gmail.com>
2025-02-06 14:13:32 -0300

    Alonso update

--- page1.py
+++ page1.py
@@ -1,18 +1,8 @@
-#import streamlit as st
-#import subprocess
-#import requests
-#import base64
-#import json
-
-import requests
 import streamlit as st
-
-url = "https://api.github.com/repos/PlannerWG/Planificador-de-Asignaturas"  # Cambia OWNER y REPO por los correctos
-
-response = requests.get(url)  # Sin autenticación
-
-st.write(f"Status Code: {response.status_code}")
-st.json(response.json())  # Ver el resultado
+import subprocess
+import requests
+import base64
+import json
 
 #####################################################################################################
 #####################################################################################################
@@ -22,7 +12,7 @@ st.json(response.json())  # Ver el resultado
 #####################################################################################################
 #####################################################################################################
 
-"""
+
 # Inicialización de GITHUB
 GITHUB_TOKEN = st.secrets["GITHUB_TOKEN"]
 headers = {
@@ -179,4 +169,4 @@ if flag:
         else:
             eng_cfg.pop(5)
     eng_cfg[0]["prerequisites"]=list()
-"""
+    
